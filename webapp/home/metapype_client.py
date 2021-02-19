5ed86e385b9290dabc5cbc4d610eb8ed8abd4e0f
jide <jride@wisc.edu>
2021-02-19 17:30:57 -0600

    Advance the release date.

--- webapp/home/metapype_client.py
+++ webapp/home/metapype_client.py
@@ -55,7 +55,7 @@ if Config.LOG_DEBUG:
 
 logger = daiquiri.getLogger('metapype_client: ' + __name__)
 
-RELEASE_NUMBER = '2021.02.18'
+RELEASE_NUMBER = '2021.02.19'
 
 NO_OP = ''
 UP_ARROW = html.unescape('&#x25B2;')

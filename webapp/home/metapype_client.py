9bdc11ead00d0a77d75bedf3d0e1320bb0f5b52d
jide <jride@wisc.edu>
2021-03-15 16:02:39 -0500

    Update User Guide to include changes to Submit to EDI and Import/Export.

--- webapp/home/metapype_client.py
+++ webapp/home/metapype_client.py
@@ -57,7 +57,7 @@ if Config.LOG_DEBUG:
 
 logger = daiquiri.getLogger('metapype_client: ' + __name__)
 
-RELEASE_NUMBER = '2021.03.12'
+RELEASE_NUMBER = '2021.03.17'
 
 NO_OP = ''
 UP_ARROW = html.unescape('&#x25B2;')

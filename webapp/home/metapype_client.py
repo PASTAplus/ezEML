3b77b79fe432076eeaa6d3d61adcf50b2b8ff60e
jide <jride@wisc.edu>
2022-03-07 14:46:24 -0600

    Add User Guide chapters for new features.

--- webapp/home/metapype_client.py
+++ webapp/home/metapype_client.py
@@ -74,7 +74,7 @@ if Config.LOG_DEBUG:
 
 logger = daiquiri.getLogger('metapype_client: ' + __name__)
 
-RELEASE_NUMBER = '2022.03.03'
+RELEASE_NUMBER = '2022.03.07'
 
 NO_OP = ''
 UP_ARROW = html.unescape('&#x25B2;')

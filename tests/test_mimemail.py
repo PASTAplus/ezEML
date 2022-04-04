eb29c1a575828554c4759176ffe131382cda6d29
Mark Servilla <mark.servilla@gmail.com>
2022-04-03 21:04:42 -0600

    Add MIME mail support as a replacement for mailout

---
+++ tests/test_mimemail.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: test_mimemail
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    10/26/20
+"""
+import logging
+import os
+
+import daiquiri
+import pytest
+
+import webapp.mimemail as mimemail
+
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + "/test_mailout.log"
+daiquiri.setup(level=logging.INFO,
+               outputs=(daiquiri.output.File(logfile), "stdout",))
+logger = daiquiri.getLogger(__name__)
+
+
+def test_send_mail():
+    subject = "Test ezEML support email notification..."
+    msg = "IGNORE -- Test ezEML support email notification. Testing UNICODE: ∑ ÿ 𘚟"
+    to = "support@environmentaldatainitiative.org"
+    mimemail.send_mail(subject, msg, to)
---
+++ webapp/mimemail.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: mimemail
+
+:Synopsis:
+    Provide MIME Multipart email support (see: https://realpython.com/python-send-email/)
+
+:Author:
+    servilla
+
+:Created:
+    4/3/22
+"""
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import daiquiri
+
+from webapp.config import Config
+
+
+logger = daiquiri.getLogger(__name__)
+
+
+def send_mail(subject, msg, to, sender_name=None, sender_email=None) -> bool:
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = Config.HOVER_MAIL
+    message["To"] = Config.HOVER_MAIL
+
+    part = MIMEText(msg, "plain")
+    message.attach(part)
+
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL("mail.hover.com", 465, context=context) as server:
+            server.login(Config.HOVER_MAIL, Config.HOVER_PASSWORD)
+            server.sendmail(
+                Config.HOVER_MAIL, Config.HOVER_MAIL, message.as_string()
+            )
+        return True
+    except Exception as e:
+        logger.error(e)
+        return False

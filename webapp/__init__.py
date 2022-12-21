dc9ac6b1f207d5ef0a1f685dbcb4e2018f19370b
jide <jride@wisc.edu>
2022-12-21 16:03:10 -0600

    Add thread name to logging entries.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -29,7 +29,7 @@ daiquiri.setup(level=logging.INFO,
                outputs=[
                    daiquiri.output.File(logfile,
                                              formatter=daiquiri.formatter.ExtrasFormatter(
-                                                fmt=("%(asctime)s [PID %(process)d] [%(levelname)s]" +
+                                                fmt=("%(asctime)s [PID %(process)d] [%(threadName)s] [%(levelname)s]" +
                                                      "%(extras)s %(name)s -> %(message)s"),
                                                 keywords=None)), 'stdout'
                 ])

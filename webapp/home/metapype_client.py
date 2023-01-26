10c65fb3fd71249064de535ec965204b9e9bdf8e
jide <jride@wisc.edu>
2023-01-25 20:55:38 -0600

    Advance the release number.

--- webapp/home/metapype_client.py
+++ webapp/home/metapype_client.py
@@ -75,7 +75,7 @@ if Config.LOG_DEBUG:
 
 logger = daiquiri.getLogger('metapype_client: ' + __name__)
 
-RELEASE_NUMBER = '2023.01.18'
+RELEASE_NUMBER = '2023.01.25'
 
 NO_OP = ''
 UP_ARROW = html.unescape('&#x25B2;')

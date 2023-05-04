0e844474b575ca489e00d175d4ec43da06e47d21
jide <jride@wisc.edu>
2023-05-04 13:11:23 -0500

    Fix GitHub issue #105.

--- webapp/home/views.py
+++ webapp/home/views.py
@@ -1182,6 +1182,8 @@ def open_eml_document():
 @home.route('/open_package/<package_name>/<owner>', methods=['GET', 'POST'])
 @login_required
 def open_package(package_name, owner=None):
+    if not owner:
+        owner = current_user.get_user_login()
     owner_data_dir = os.path.join(Config.USER_DATA_DIR, owner)
 
     eml_node = load_eml(package_name, folder_name=owner_data_dir)

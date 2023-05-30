91f0ef6ebc6b1295ea1af798fc40feddb5814fa1
jide <jride@wisc.edu>
2023-05-30 18:00:02 -0500

    Fuss with datetime parsing in load csv.

--- webapp/home/check_data_table_contents.py
+++ webapp/home/check_data_table_contents.py
@@ -96,7 +96,7 @@ def load_df(eml_node, csv_url, data_table_name):
 
     return pd.read_csv(csv_url, encoding='utf-8-sig', sep=delimiter, quotechar=quote_char,
                        keep_default_na=False, skiprows=range(1, num_header_lines),
-                       skipfooter=num_footer_lines, low_memory=False)
+                       skipfooter=num_footer_lines, low_memory=False, infer_datetime_format=True)
 
 
 def find_data_table_node(eml_node, data_table_name):

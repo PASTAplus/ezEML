ed36e0a76da3ed73985fd44b7abd63b4cf858cd7
jide <jride@wisc.edu>
2023-06-20 15:28:17 -0500

    Set the version number.

--- webapp/home/metapype_client.py
+++ webapp/home/metapype_client.py
@@ -75,7 +75,7 @@ if Config.LOG_DEBUG:
 
 logger = daiquiri.getLogger('metapype_client: ' + __name__)
 
-RELEASE_NUMBER = '2023.06.15'
+RELEASE_NUMBER = '2023.06.20'
 
 NO_OP = ''
 UP_ARROW = html.unescape('&#x25B2;')

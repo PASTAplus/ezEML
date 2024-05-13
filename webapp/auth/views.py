e5f52b576b5978998dacb455f67f223f3fa10fb9
jide <jride@wisc.edu>
2024-05-13 18:57:23 -0500

    Add debug info.

--- webapp/auth/views.py
+++ webapp/auth/views.py
@@ -60,7 +60,7 @@ def login():
             return redirect(url_for(PAGE_INDEX))
 
     log_info(f"request.url: {request.url}")
-    log_info(f"request.args: {request.args}")
+    log_info(f"request.args: {request.args.to_dict()}")
 
     # Process POST
     form = LoginForm()

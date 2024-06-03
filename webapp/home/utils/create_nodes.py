682142293c34dcb6d8632c917b8f8c45a543e92a
jide <jride@wisc.edu>
2024-06-03 00:11:31 -0500

    Fix method step description with invalid XML.

--- webapp/home/utils/create_nodes.py
+++ webapp/home/utils/create_nodes.py
@@ -1105,6 +1105,12 @@ def create_method_step(method_step_node:Node=None, description:str=None, instrum
                 description = ''  # TODO: Handle cases with empty description but non-empty data_sources
             description = f"{description}\n{data_sources_marker_begin}\n{data_sources}\n{data_sources_marker_end}"
 
+        valid, msg = texttype_node_processing.is_valid_xml_fragment(description, names.DESCRIPTION)
+        if not valid:
+            log_error(msg)
+            flash(texttype_node_processing.invalid_xml_error_message(msg), 'error')
+            return False
+
         texttype_node_processing.post_process_texttype_node(description_node, description)
 
         instrumentation_nodes = method_step_node.find_all_children(names.INSTRUMENTATION)
@@ -1131,6 +1137,7 @@ def create_method_step(method_step_node:Node=None, description:str=None, instrum
         else:
             for instrumentation_node in instrumentation_nodes:
                 method_step_node.remove_child(instrumentation_node)
+    return True
 
 
 def create_data_source(data_source_node:Node=None, title:str=None, online_description:str=None, online_url:str=None):

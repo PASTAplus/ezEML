7ec94f2d292ff4bc81c68e8a70ca1d101fdba859
jide <jride@wisc.edu>
2024-06-05 16:16:06 -0500

    Log ezEML username on login

--- webapp/auth/views.py
+++ webapp/auth/views.py
@@ -87,7 +87,7 @@ def login():
             user = User(session_id)
             login_user(user)
             initialize_user_data(cname, pasta_token.uid, auth_token)
-            log_usage(actions['LOGIN'], cname, 'LDAP')
+            log_usage(actions['LOGIN'], cname, 'LDAP', current_user.get_user_login())
             next_page = request.args.get('next')
             if not next_page or urlparse(next_page).netloc != '':
                 current_document = get_active_document()

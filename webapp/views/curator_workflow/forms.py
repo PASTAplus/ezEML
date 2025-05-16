33a709da42ac76c7e12e50acf49b41f1099d193f
jide <jride@wisc.edu>
2025-05-16 12:24:15 -0500

    First work on expanding curator workflow to other scopes

--- webapp/views/curator_workflow/forms.py
+++ webapp/views/curator_workflow/forms.py
@@ -2,7 +2,7 @@
 from flask_wtf import FlaskForm
 
 from wtforms import (
-    StringField, RadioField, SubmitField
+    StringField, RadioField, SubmitField, SelectField
 )
 from wtforms.widgets import TextArea
 
@@ -14,6 +14,42 @@ from webapp.home.forms import EDIForm
 
 class CuratorWorkflowForm(FlaskForm):
                                            # validators=[Optional()])
+
+    scope = SelectField('Scope',
+                       choices=[("edi", "edi"),
+                                ("knb-lter-and", "knb-lter-and"),
+                                ("knb-lter-arc", "knb-lter-arc"),
+                                ("knb-lter-bes", "knb-lter-bes"),
+                                ("knb-lter-ble", "knb-lter-ble"),
+                                ("knb-lter-bnz", "knb-lter-bnz"),
+                                ("knb-lter-cap", "knb-lter-cap"),
+                                ("knb-lter-cce", "knb-lter-cce"),
+                                ("knb-lter-cdr", "knb-lter-cdr"),
+                                ("knb-lter-cwt", "knb-lter-cwt"),
+                                ("knb-lter-fce", "knb-lter-fce"),
+                                ("knb-lter-gce", "knb-lter-gce"),
+                                ("knb-lter-hbr", "knb-lter-hbr"),
+                                ("knb-lter-hfr", "knb-lter-hfr"),
+                                ("knb-lter-jrn", "knb-lter-jrn"),
+                                ("knb-lter-kbs", "knb-lter-kbs"),
+                                ("knb-lter-knz", "knb-lter-knz"),
+                                ("knb-lter-luq", "knb-lter-luq"),
+                                ("knb-lter-mcm", "knb-lter-mcm"),
+                                ("knb-lter-mcr", "knb-lter-mcr"),
+                                ("knb-lter-msp", "knb-lter-msp"),
+                                ("knb-lter-nes", "knb-lter-nes"),
+                                ("knb-lter-nga", "knb-lter-nga"),
+                                ("knb-lter-nin", "knb-lter-nin"),
+                                ("knb-lter-ntl", "knb-lter-ntl"),
+                                ("knb-lter-nwk", "knb-lter-nwk"),
+                                ("knb-lter-nwt", "knb-lter-nwt"),
+                                ("knb-lter-pal", "knb-lter-pal"),
+                                ("knb-lter-pie", "knb-lter-pie"),
+                                ("knb-lter-sbc", "knb-lter-sbc"),
+                                ("knb-lter-sev", "knb-lter-sev"),
+                                ("knb-lter-sgs", "knb-lter-sgs"),
+                                ("knb-lter-vcr", "knb-lter-vcr")
+                                ])
 
     new_or_existing = RadioField('Data Package ID',
                                  choices=[('New', 'Get a new Package ID from PASTA'),

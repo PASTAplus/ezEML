9a0c589880a27f403f60fcc8215637e13ab40f15
jide <jride@wisc.edu>
2025-06-17 17:47:38 -0500

    Add debug info

--- webapp/home/utils/security.py
+++ webapp/home/utils/security.py
@@ -22,9 +22,11 @@ def validate_download_url(url):
     # Case 1: Allow file:// URLs, but restrict to a safe base directory
     if parsed_url.scheme == "file":
         local_path = os.path.abspath(os.path.join(parsed_url.netloc, parsed_url.path))
+        log_info(f"local_path: {local_path}")
         if not local_path.startswith(allowed_file_base):
             flash(f"Local file access is not allowed outside {Config.BASE_DIR}.", "error")
             return None
+        log_info(f"returning: {url}")
         return url
 
     # Case 2: Regular HTTP(S) URLs
